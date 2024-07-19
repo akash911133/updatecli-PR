218a3bc47850a1f93acfdc31329c32323cc456c6
aughade <akashughade77@gmail.com>
2024-07-19 18:00:20 +0530

    feature added

---
+++ templates/eks/input.tf
@@ -0,0 +1,39 @@
+variable "AWS_LOAD_BALANCER_CONTROLLER_VERSION" {
+  type    = string
+  default = "1.8.1"
+}
+
+variable "CLUSTER_AUTOSCALER_HELM_CHART_VERSION" {
+  type    = string
+  default = "9.37.0"
+}
+
+variable "EFS_DRIVER_CHART_VERSION" {
+  type    = string
+  default = "3.0.6"
+}
+
+variable "NGINX_CHART_VERSION" {
+  type    = string
+  default = "4.11.1"
+}
+
+variable "METRICS_SERVER_VERSION" {
+  type    = string
+  default = "3.12.1"
+}
+
+variable "CERTMANAGER_VERSION" {
+  type    = string
+  default = "v1.15.1"
+}
+
+variable "DATADOG_CRDS_VERSION" {
+  type    = string
+  default = "1.7.0"
+}
+
+variable "DATADOG_VERSION" {
+  type    = string
+  default = "3.68.1"
+}
